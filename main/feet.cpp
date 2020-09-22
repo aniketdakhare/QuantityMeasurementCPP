5250d500bc2194475531e95727cdf38e7c5ed7d7
“Aniket_D” <aniketdakhare@gmail.com>
2020-09-22 23:40:33 +0530

    ADDED Code and Test case to get true value if  0 feet equals to 0 feet.

---
+++ main/feet.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "feet.h"
+
+Feet::Feet(double value)
+{
+        this->value = value;
+}
+
+bool Feet::operator== (Feet other) const 
+{
+        return (this->value == other.value);
+}
