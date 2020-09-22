341828ab9ffd6e9db85de288c563a3e93af3f120
“Aniket_D” <aniketdakhare@gmail.com>
2020-09-22 23:59:24 +0530

    ADDED Code and Test case to do null check.

--- main/feet.h
+++ main/feet.h
@@ -1,4 +1,4 @@
-#include <iostream>
+#pragma once
 using namespace std;
 
 class Feet 
@@ -9,4 +9,7 @@ public:
     Feet(double value);
    
     bool operator== (Feet) const;
+    bool operator== (Feet *other) const;
+    bool operator!= (Feet* other) const;
+    bool operator!= (Feet other) const;
 };
--- test/feetTest.cpp
+++ test/feetTest.cpp
@@ -1,10 +1,16 @@
 #include "../main/feet.h"
 #include <gtest/gtest.h>
 
-TEST(FeetTests, _0feet_and_0feet__should_be_equal) 
+TEST(FeetTest, _0feet_and_0feet__should_be_equal) 
 { 
     Feet first_zero_feet(0), second_zero_feet(0); 
     ASSERT_EQ(first_zero_feet, second_zero_feet);
+}
+
+TEST(FeetTest, _null_feet_and_1feet__should_not_be_equal) 
+{
+    Feet first_zero_feet(1);
+    ASSERT_NE(first_zero_feet, nullptr);
 }
 
 int main(int argc, char **argv) 
